6625958c86f618680c085f1a19802355e8581b97
tsugumi-sys <tidemark0105@gmail.com>
2023-12-19 01:51:45 +0900

    fix pp

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="stocklake",
-    version="0.1.0",
+    version="0.0.3",
     py_modules=["stocklake"],
     install_requires=[
         "Click",
     ],
+    packages=setuptools.find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     entry_points={
