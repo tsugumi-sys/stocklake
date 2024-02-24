51ddaa12e8105bccaed34fe0f0df7c37a0e36caf
tsugumi-sys <tidemark0105@gmail.com>
2024-02-24 23:31:12 +0900

    ruff

---
+++ stocklake/environment_variables.py
@@ -0,0 +1,29 @@
+import os
+from typing import Any
+
+
+class _EnvironmentVariable:
+    """Represents an environment variable"""
+
+    def __init__(self, name: str, default: Any):
+        self.name = name
+        self.default = default
+
+    @property
+    def defined(self):
+        return self.name in os.environ
+
+    def get(self):
+        if (val := os.getenv(self.name)) is not None:
+            return val
+        return self.default
+
+
+STOCKLAKE_POSTGRES_HOST = _EnvironmentVariable("STOCKLAKE_POSTGRES_HOST", "localhost")
+STOCKLAKE_POSTGRES_USER = _EnvironmentVariable("STOCKLAKE_POSTGRES_USER", "postgres")
+STOCKLAKE_POSTGRES_PASSWORD = _EnvironmentVariable(
+    "STOCKLAKE_POSTGRES_PASSWOR", "password"
+)
+STOCKLAKE_POSTGRES_DATABASE = _EnvironmentVariable(
+    "STOCKLAKE_POSTGRES_DATABASE", "postgres"
+)
---
+++ stocklake/stores/db/utils.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+from stocklake.environment_variables import (
+    STOCKLAKE_POSTGRES_DATABASE,
+    STOCKLAKE_POSTGRES_HOST,
+    STOCKLAKE_POSTGRES_PASSWORD,
+    STOCKLAKE_POSTGRES_USER,
+)
+
+
+def create_sqlalchemy_engine() -> Engine:
+    return create_engine(
+        f"postgresql://{STOCKLAKE_POSTGRES_USER.get()}:{STOCKLAKE_POSTGRES_PASSWORD.get()}@{STOCKLAKE_POSTGRES_HOST.get()}/{STOCKLAKE_POSTGRES_DATABASE.get()}",
+        connect_args={"check_same_thread": False},
+    )
