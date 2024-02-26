6c148c59c9167315d376d39573a2337cc450343a
Akira Noda <akira.noda@onecareer.com>
2024-02-26 16:23:01 +0900

    Adding nasdaqapi table

--- stocklake/stores/db_migrations/env.py
+++ stocklake/stores/db_migrations/env.py
@@ -3,6 +3,10 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from db.base_sql_model import Base
+
+# from ..db.models import NasdaqStock
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -16,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
