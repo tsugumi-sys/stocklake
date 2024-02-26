473976fdcb6a905ccccc5216a02ebe383f06ed73
Akira Noda <akira.noda@onecareer.com>
2024-02-26 16:25:18 +0900

    fix lint error

--- stocklake/stores/db_migrations/env.py
+++ stocklake/stores/db_migrations/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
-
 from db.base_sql_model import Base
+from sqlalchemy import engine_from_config, pool
 
 # from ..db.models import NasdaqStock
 
