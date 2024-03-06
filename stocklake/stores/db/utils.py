1dc795eccfe2bd7ba4c2c516d20109d26a4731d3
tsugumi-sys <tidemark0105@gmail.com>
2024-03-07 01:27:09 +0900

    remove unuse code

--- stocklake/stores/db/utils.py
+++ stocklake/stores/db/utils.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 from alembic.command import upgrade
 from alembic.config import Config
-from alembic.migration import MigrationContext
-from alembic.script import ScriptDirectory
 from sqlalchemy.engine.base import Engine
 
 
@@ -29,12 +27,3 @@ def migrate(engine: Engine, revision: str) -> None:
     with engine.begin() as conn:
         alembic_cfg.attributes["connection"] = conn
         upgrade(alembic_cfg, revision)
-
-
-def migrate_if_needed(engine: Engine, revision: str) -> None:
-    alembic_cfg = _get_alembic_config(engine.url.render_as_string(hide_password=False))
-    script_dir = ScriptDirectory.from_config(alembic_cfg)
-    with engine.begin() as conn:
-        context = MigrationContext(conn)
-        if context.get_current_revision() != script_dir.get_current_head():
-            upgrade(alembic_cfg, revision)
