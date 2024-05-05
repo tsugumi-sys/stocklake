89b2653f731f8991bbba42cbcdc07b8acd52dd5d
tsugumi-sys <tidemark0105@gmail.com>
2024-05-05 18:52:31 +0900

    Store: Delete per exchange data nasdaqapi

--- stocklake/nasdaqapi/stores.py
+++ stocklake/nasdaqapi/stores.py
@@ -28,25 +28,28 @@ class NASDAQDataStore(BaseStore):
     def save(
         self,
         store_type: StoreType,
-        exchange_name: Exchange,
+        exchange: Exchange,
         data: List[NasdaqApiSymbolData],
     ):
         if store_type == StoreType.LOCAL_ARTIFACT:
             repository = LocalArtifactRepository(SAVE_ARTIFACTS_DIR)
             with tempfile.TemporaryDirectory() as tmpdir:
-                csv_file_path = os.path.join(tmpdir, f"{exchange_name}_data.csv")
+                csv_file_path = os.path.join(tmpdir, f"{exchange}_data.csv")
                 save_data_to_csv(data, csv_file_path)
                 repository.save_artifact(csv_file_path)
         if store_type == StoreType.POSTGRESQL:
             if self.sqlalchemy_session is None:
                 raise StockLoaderException("`sqlalchemy_session` is None.")
-            sqlstore = NasdaqApiSQLAlchemyStore(self.sqlalchemy_session)
+            sqlstore = NasdaqApiSQLAlchemyStore(exchange, self.sqlalchemy_session)
             sqlstore.delete()
             sqlstore.create([schemas.NasdaqStockCreate(**d) for d in data])
 
 
 class NasdaqApiSQLAlchemyStore(SQLAlchemyStore):
-    def __init__(self, session: orm.sessionmaker[orm.session.Session]):
+    def __init__(
+        self, exchange: Exchange, session: orm.sessionmaker[orm.session.Session]
+    ):
+        self.exchange = exchange
         self.session = session
 
     def create(self, data: schemas.NasdaqStockCreate | List[schemas.NasdaqStockCreate]):
@@ -64,4 +67,6 @@ class NasdaqApiSQLAlchemyStore(SQLAlchemyStore):
 
     def delete(self):
         with self.session() as session, session.begin():
-            session.query(models.NasdaqApiData).delete()
+            session.query(models.NasdaqApiData).filter(
+                models.NasdaqApiData.exchange == self.exchange
+            ).delete()
