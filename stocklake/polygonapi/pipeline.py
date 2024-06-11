e5c08f9cfd83f45386220f3a4f1a8a64cdbafe56
tsugumi-sys <tidemark0105@gmail.com>
2024-06-12 00:01:50 +0900

    Pipeline: Adding polygon financials pipeline

---
+++ stocklake/polygonapi/pipeline.py
@@ -0,0 +1,67 @@
+import logging
+from typing import List
+
+from sqlalchemy import orm
+
+from stocklake.core.base_data_loader import BaseDataLoader
+from stocklake.core.base_pipeline import BasePipeline
+from stocklake.core.base_preprocessor import BasePreprocessor
+from stocklake.core.base_store import BaseStore
+from stocklake.core.stdout import PipelineStdOut
+from stocklake.exceptions import StockLoaderException
+from stocklake.polygonapi.data_loader import (
+    PolygonFinancialsDataLoader,
+)
+from stocklake.polygonapi.preprocessor import (
+    PolygonFinancialsDataPreprocessor,
+)
+from stocklake.polygonapi.stores import PolygonFinancialsDataStore
+from stocklake.stores.constants import StoreType
+from stocklake.stores.db.database import LocalSession  # noqa: E402
+
+logger = logging.getLogger(__name__)
+
+
+class PolygonFinancialsDataPipeline(BasePipeline):
+    def __init__(
+        self,
+        symbols: List[str],
+        skip_download: bool = False,
+        store_type: StoreType = StoreType.LOCAL_ARTIFACT,
+        sqlalchemy_session: orm.sessionmaker[orm.session.Session] = LocalSession,
+    ):
+        self.symbols = symbols
+        self.skip_download = skip_download
+
+        if store_type not in StoreType.types():
+            raise StockLoaderException(
+                f"Specified store type is invalid, {store_type}, valid types are {StoreType.types()}"
+            )
+        self.store_type = store_type
+        self.data_loader = PolygonFinancialsDataLoader()
+        self.preprocessor = PolygonFinancialsDataPreprocessor()
+        self.store = PolygonFinancialsDataStore(sqlalchemy_session)
+        self.stdout = PipelineStdOut()
+
+    def run(self):
+        for symbol in self.symbols:
+            self._run(symbol, self.data_loader, self.preprocessor, self.store)
+
+    def _run(
+        self,
+        symbol: str,
+        data_loader: BaseDataLoader,
+        preprocessor: BasePreprocessor,
+        store: BaseStore,
+    ):
+        self.stdout.starting(f"Polygon Finaqncials API of {symbol}")
+        if not self.skip_download:
+            self.stdout.downloading()
+            raw_data = data_loader.download()
+        else:
+            self.stdout.skip_downloading()
+            # TODO: fetch from cached file
+            return
+        data = preprocessor.process(raw_data)
+        store.save(self.store_type, data)
+        self.stdout.completed()
