08d9e97be0dd4a80b443fbce5cc1ac9861711dc1
tsugumi-sys <tidemark0105@gmail.com>
2024-07-07 11:23:30 +0900

    Store: Implement postgresql store for wiki sp500

--- stocklake/wiki_sp500/stores.py
+++ stocklake/wiki_sp500/stores.py
@@ -2,27 +2,31 @@ import os
 import tempfile
 from typing import List, Optional
 
+from stocklake.core.base_sqlalchemy_store import SQLAlchemyStore
 from stocklake.core.base_store import BaseStore
 from stocklake.core.constants import DATA_DIR
 from stocklake.stores.artifact.local_artifact_repo import LocalArtifactRepository
 from stocklake.stores.constants import StoreType
+from stocklake.stores.db import models
 from stocklake.stores.db.database import (
     DATABASE_SESSION_TYPE,
     local_session,
 )
 from stocklake.utils.file_utils import save_data_to_csv
-from stocklake.wiki_sp500.entities import PreprocessedWikiSp500Data
+from stocklake.wiki_sp500 import entities
 
 SAVE_ARTIFACTS_DIR = os.path.join(DATA_DIR, "wiki_sp500")
 
 
-class WikiSP500Stores(BaseStore):
+class WikiSP500Store(BaseStore):
     def __init__(self, sqlalchemy_session: Optional[DATABASE_SESSION_TYPE] = None):
         if sqlalchemy_session is None:
             sqlalchemy_session = local_session()
         self.sqlalchemy_session = sqlalchemy_session
 
-    def save(self, store_type: StoreType, data: List[PreprocessedWikiSp500Data]) -> str:
+    def save(
+        self, store_type: StoreType, data: List[entities.PreprocessedWikiSp500Data]
+    ) -> str:
         if store_type == StoreType.LOCAL_ARTIFACT:
             repository = LocalArtifactRepository(SAVE_ARTIFACTS_DIR)
             with tempfile.TemporaryDirectory() as tmpdir:
@@ -34,3 +38,22 @@ class WikiSP500Stores(BaseStore):
             raise NotImplementedError()
         else:
             raise NotImplementedError()
+
+
+class WikiSP500DataSQLAlchemyStore(SQLAlchemyStore):
+    def __init__(self, session: DATABASE_SESSION_TYPE):
+        self.session = session
+
+    def create(self, data: List[entities.WikiSP500DataCreate]):
+        with self.session() as session, session.begin():
+            session.add_all([models.WikiSP500Data(**d.model_dump()) for d in data])
+
+    def read(self):
+        raise NotImplementedError()
+
+    def update(self):
+        raise NotImplementedError()
+
+    def delete(self):
+        with self.session() as session, session.begin():
+            session.query(models.WikiSP500Data).delete()
