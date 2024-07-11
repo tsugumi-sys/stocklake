17beb18f23b044fbcd4291df0b630d43b1f2028f
tsugumi-sys <tidemark0105@gmail.com>
2024-07-11 21:34:45 +0900

    Preprocessor: Implement polygonapi aggregate bars preprocessor

---
+++ stocklake/polygonapi/aggregates_bars/entities.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class PolygonAggregatesBarsDataBase(BaseModel):
+    ticker: str
+    timestamp_ms: int
+    open: float
+    high: float
+    low: float
+    close: float
+    transactions: int
+    volume: int
+    volume_weighted_average_price: float
+
+
+class PreprocessedPolygonAggregatesBarsData(PolygonAggregatesBarsDataBase):
+    pass
